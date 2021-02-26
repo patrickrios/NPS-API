d11f4ed2a19cc052e7b4de37eee28e13a782ece7
Patrick Rios <patrick.33.rios@hotmail.com>
2021-02-26 18:42:30 -0400

    Save user answer

---
+++ src/controllers/AnswerController.ts
@@ -0,0 +1,43 @@
+import {Request, response, Response} from 'express'
+import { getCustomRepository } from 'typeorm';
+import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+
+
+class AnswerController{
+
+    async execute(request: Request, reponse: Response){
+        const {value} = request.params;
+        const {u} = request.query
+
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
+
+        const surveyUser = await surveysUsersRepository.findOne({
+            id: String(u)
+        })
+
+        if(!surveyUser){
+            return reponse.status(400).json({
+                error: "Survey User does not exists!"
+            })
+        }
+
+        surveyUser.value = Number(value)
+        await surveysUsersRepository.save(surveyUser)
+
+        return response.json(surveyUser)
+    }
+
+}
+
+export {AnswerController}
+
+
+
+/**
+ * Route params -> Fazem parte da rota
+ * routes.get("/answers/:value")
+ * 
+ * Query Paramns -> Busca, paginação, não obrigatoria
+ * ?
+ * chave=valor
+*/
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+import { AnswerController } from './controllers/AnswerController'
 import { SendMailConroller } from './controllers/SendMailController'
 import { SurveysController } from './controllers/SurveysController'
 import {UserController} from './controllers/UserController'
@@ -13,6 +14,7 @@ const router = Router()
 const userController = new UserController()
 const surveysController = new SurveysController()
 const sendMailController = new SendMailConroller()
+const answerController = new AnswerController()
 
 /** Ao receber dados pelo método POST
  *  na rota '/users' usa o método 
@@ -24,5 +26,7 @@ router.post('/surveys', surveysController.create)
 router.get('/surveys', surveysController.show)
 
 router.post('/sendMail', sendMailController.execute )
+
+router.get('/answers/:value', answerController.execute)
 
 export {router}
